b1a08c84a1fc64342396855e52be0732af01534b
Simon Sinding <sinding2000@gmail.com>
2018-05-23 02:25:28 +0200

    added looking for token

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-  name: 'App'
-}
+  name: 'App',
+  created() {
+    const token = this.$q.localStorage.get.item('feathers-jwt');
+    if (token === null || token === undefined) return;
+    this.$store.dispatch('auth/authenticate', token);
+  }
+};
 </script>
 
 <style>
